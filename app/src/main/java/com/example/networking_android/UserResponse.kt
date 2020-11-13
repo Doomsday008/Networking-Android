b0926b7ac017cd958eb83291f3105ac8b7a3fb85
kamal.knt66@gmail.com <kamal.knt66@gmail.com>
2020-11-13 18:27:04 +0530

    addingsearchUser

---
+++ app/src/main/java/com/example/networking_android/UserResponse.kt
@@ -0,0 +1,7 @@
+package com.example.networking_android
+
+data class UserResponse(
+        val totalCount: Int? = null,
+        val incompleteResults: Boolean? = null,
+        val items: List<User>? = null
+)
